71550617457d6bdcc000e7f7fd4b37601435224f
Jordy Mateo <jordymateo@hotmail.es>
2019-05-25 22:50:04 -0400

    initial commit

---
+++ 2019-05-24/StringCalculator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba los numberos que desea sumar: ");
+            var input = Console.ReadLine();
+
+            var stringCalculator = new StringCalculator();
+            var output = stringCalculator.add(input);
+
+            Console.WriteLine($"El resultado es: {output}");
+            Console.ReadKey();
+        }
+
+
+
+    }
+
+    class StringCalculator
+    {
+        public string add(string number)
+        {
+            //if (string.IsNullOrWhiteSpace(number))
+            //    return "";
+
+            string[] numbers = number.Split(',');
+            string errors = "";
+            
+            double sum = 0;
+
+            foreach (var num in numbers)
+            {
+
+                if (num.IndexOf('\n') > -1)
+                {
+                    if (num.IndexOf('\n') > 2)
+                    {
+                        errors += $"Number expected but '\n' found at position {number.IndexOf(num)}";
+                        continue;
+                    }
+
+                    var listSubNums = num.Split(new[] { @"\\n" }, StringSplitOptions.None);
+
+                    foreach (var subNum in listSubNums)
+                    {
+                        sum += int.Parse(num);
+                    }
+                }
+
+                sum += int.Parse(num);
+            }
+
+            if (errors.Count() > 0)
+                return errors;
+            //foreach(var number )
+            return sum.ToString();
+        }
+    }
+}
---
+++ 2019-05-25/2019-05-25/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+namespace _2019_05_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite los numeros:");
+            var number = Console.ReadLine();
+
+            Console.WriteLine(add(number));
+
+            Console.ReadKey();
+        }
+
+        static string add(string number)
+        {
+            if (string.IsNullOrWhiteSpace(number))
+                return "0";
+
+            string separator = ",";
+            int position = 0;
+            int sum = 0;
+            string errors = "";
+
+            if (number.StartsWith("//"))
+            {
+                number = number.Replace("\\n", "\n");
+                var separatorIndex = number.IndexOf("\n");
+                separator = number.Substring(0, separatorIndex);
+                separator = separator.Replace("//", "");
+
+                int numberLength = number.Count() - (separatorIndex + 1);
+                number = number.Substring(separatorIndex + 1, numberLength);
+                position += 5;
+            }
+
+            var numbers = number.Split(separator);
+
+            foreach (var num in numbers)
+            {
+                var xNum = num.Replace("\\n", "\n");
+
+                if (string.IsNullOrWhiteSpace(xNum))
+                {
+                    errors += "Number expected but EOF found. \n";
+                    continue;
+                }
+
+
+                if (xNum.IndexOf('\n') > -1)
+                {
+
+                    if (xNum.StartsWith('\n'))
+                    {
+                        errors = $"Number expected but '\\n' found at position {position}. \n";
+                        continue;
+                    }
+
+                    var subNumbers = xNum.Split('\n');
+
+                    foreach(var sn in subNumbers)
+                    {
+                        sum += int.Parse(sn);
+                        position += sn.Count() + 1;
+                    }
+                    continue;
+                }
+
+                sum += int.Parse(xNum);
+                position += xNum.Count() + 1;
+            }
+
+            if (!string.IsNullOrWhiteSpace(errors))
+                return errors;
+
+            return sum.ToString();
+        }
+    }
+}
